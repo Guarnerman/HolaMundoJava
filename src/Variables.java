4578d11bcbaa6b93e10f6a69d90f933dd7bc78a1
Guarnerman <gs.ulises18@gmail.com>
2023-11-20 19:58:00 -0600

    Segundo commit: declaración e imprimir variables

---
+++ src/Variables.java
@@ -0,0 +1,11 @@
+public class Variables {
+    public static void main(String[] args) {
+
+        int speed = 0;
+        System.out.println(speed);
+
+        int salary = 1000;
+        String employeeName = "Votitikzo";
+        System.out.println(employeeName);
+    }
+}
