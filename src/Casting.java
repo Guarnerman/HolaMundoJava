6c836d324bfbd95b12e8c2ebad51d8b4dfcc7268
guarnerman <gs.ulises18@gmail.com>
2023-11-22 00:02:47 -0600

    Casting de variables Intro

---
+++ src/Casting.java
@@ -0,0 +1,14 @@
+public class Casting {
+    public static void main(String[] args) {
+        double monthlyDogs = 30.0/12.0;
+        System.out.println(monthlyDogs);
+
+        int estimatedMonthlyDogs = (int) monthlyDogs;
+        System.out.println(estimatedMonthlyDogs);
+
+        int a = 30;
+        int b = 12;
+
+        System.out.println(a/b);
+    }
+}
