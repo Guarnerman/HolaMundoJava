63f151e69fa484fb8218715e5c332312651087f9
guarnerman <gs.ulises18@gmail.com>
2023-11-21 22:59:14 -0600

    NamingJava v2

--- src/NamingJava.java
+++ src/NamingJava.java
@@ -1,7 +1,23 @@
 public class NamingJava {
     public static void main(String[] args) {
         int cellphone = 22227777;
+        int cellPhone = 55556666;
 
         System.out.println(cellphone);
+        System.out.println(cellPhone);
+
+        String $countryName = "Laos";
+        String _backgroundColor = "Blue";
+
+        String currency$ = "MXN";
+        String background_color = "BLUE";
+
+        int POSITION = -5;
+        int MAX_WIDTH = 9999;
+        int MIN_WIDTH = 1;
+
+        //Lower Camel Case
+        String fullName = "Paquita Cabeza";
+        int sizeInCentimeters = 26;
     }
 }
