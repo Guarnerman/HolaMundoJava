ee40596b70624f18c56b415d5e29613cfb593ed3
guarnerman <gs.ulises18@gmail.com>
2023-11-22 23:11:32 -0600

    Ciclo For: puede ser fori / forEach

---
+++ src/ForLoop.java
@@ -0,0 +1,21 @@
+public class ForLoop {
+    static boolean isTurnOnLight = false;
+    
+    public static void main(String[] args) {
+        
+        turnOnOffLight();
+
+        for (int j = 0; j <= 10 ; j++) {
+            printSOS();
+        }
+    }
+
+    public static void  printSOS(){
+        System.out.println("...___...");
+    }
+
+    public static boolean turnOnOffLight(){
+        isTurnOnLight = !isTurnOnLight;
+        return  isTurnOnLight;
+    }
+}
